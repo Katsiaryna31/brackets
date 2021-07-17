14918f60c723b1ea1de4c4c45a1085c4ab949370
Katsiaryna31 <katia310790@mail.ru>
2021-07-17 21:38:32 +0300

    add ready task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
     var array = str.split('');
-    if (array.length % 2 !==0) {
-      return false;
-    } else {
-      return true;
-    }
-
+    var newArr = [];
+    array.forEach(el=>{
+        if (newArr[newArr.length-1] === "(" && el === ")") {
+          newArr.pop();
+        } else if (newArr[newArr.length-1] === "[" && el === "]") {
+          newArr.pop();
+        } else if (newArr[newArr.length-1] === "{" && el === "}") {
+          newArr.pop();
+        } else if (newArr[newArr.length-1] === "|" && el === "|") {
+          newArr.pop();
+        } else if (newArr[newArr.length-1] === "1" && el === "2") {
+          newArr.pop();
+        } else if (newArr[newArr.length-1] === "3" && el === "4") {
+          newArr.pop();
+        }else if (newArr[newArr.length-1] === "5" && el === "6") {
+          newArr.pop();
+        }else if (newArr[newArr.length-1] === "7" && el === "7") {
+          newArr.pop();
+        }else if (newArr[newArr.length-1] === "8" && el === "8") {
+          newArr.pop();
+        }else {
+          newArr.push(el);
+        }
+      })
+      console.log(newArr);
+      if (newArr.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
 }
